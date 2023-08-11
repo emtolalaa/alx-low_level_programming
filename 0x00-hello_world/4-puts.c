27954b6bfe6abe63e92b5d71405cb20e0032fc2c
emtolalaa <emto.tochy@gmail.com>
2023-08-10 17:16:34 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Using the puts function to print out a text output
+ * Return: 0 (Task Done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
