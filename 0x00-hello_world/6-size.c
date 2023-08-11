fb1ecd50bf708a27d8f8d9f1ffc19399b6337ae5
emtolalaa <emto.tochy@gmail.com>
2023-08-10 18:54:41 -0700

    Core Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Printing sizes
+ *
+ * Return: 0 Task done
+ */
+
+int main(void)
+{
+	printf("Size of a char: %2f byte(s)", sizeof(char));
+	printf("Size of int: %2f byte(s)", sizeof(int));
+	printf("Size of a long int: %2f byte(s)", sizeof(long int));
+	printf("Size of float: %2f byte(s)", sizeof(float));
+	return (0);
