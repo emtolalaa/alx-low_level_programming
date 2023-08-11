415bcc8cfddf67b2b41224b6888af73955689bf1
emtolalaa <emto.tochy@gmail.com>
2023-08-10 18:33:32 -0700

    Printf at work

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Using the printf funcction in C programming
+ *
+ * Return: 0 Task Done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
