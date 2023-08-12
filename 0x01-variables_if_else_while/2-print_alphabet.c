e91a2125dfcff92676309a35ca6effcc195679a1
emtolalaa <emto.tochy@gmail.com>
2023-08-11 19:28:22 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
-#
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 Task Done
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
