d539cdb2e4b227b18bfea6d3a38586331c8e2286
emtolalaa <emto.tochy@gmail.com>
2023-08-11 21:06:37 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main fucntion
+ *
+ * Return: 0 For Task Done
+ */
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = 0; d1 < 9; d1++)
+	{
+		for (d2 = d1 + 1; d2 < 10; d2++)
+		{
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
+
+			if (d1 == 8 && d2 == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
