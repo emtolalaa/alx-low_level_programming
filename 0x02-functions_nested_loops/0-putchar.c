a72c249f5920d9198ed8461bdffc596035d55f84
emtolalaa <emto.tochy@gmail.com>
2023-08-15 19:00:30 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *   main - Entry point
+ *
+ *   Return: 0 For Task Done
+ *
+ */
+int main(void)
+
+{
+	printf("_putchar\n");
+
+	return (0);
+}
