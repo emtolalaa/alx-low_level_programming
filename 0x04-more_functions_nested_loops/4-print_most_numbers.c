8b962773b7d0132b851415bdd96d0f1251cfcfef
emtolalaa <emto.tochy@gmail.com>
2023-08-17 19:25:50 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_most_numbers _ prints numbers between 0 and 9 but not 2 and 4
+*
+* Return: returns 0 for task done
+*/
+
+void print_most_numbers(void)
+{
+
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+
+		_putchar(i + '0');
+		}
+	}
+		_putchar('\n');
+}
