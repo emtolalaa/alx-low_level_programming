bc4c63d60af242fd004462e790fd3a54b2004662
emtolalaa <emto.tochy@gmail.com>
2023-08-17 21:56:57 -0700

    Task updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,13 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (b = 0; b <= 15; b++)
 		{
-			if (b > 9)
+			if (b >= 10)
 			{
-				_putchar((b / 10) + 'O');
+				_putchar((b / 10) + '0');
 			}
-				_putchar((b % 10) + 'O');
+				_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
