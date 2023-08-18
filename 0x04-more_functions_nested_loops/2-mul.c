265315092d6a03ced70fb7c054d531aee1a450f9
emtolalaa <emto.tochy@gmail.com>
2023-08-17 18:49:30 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @x : First integer
+* @y : second integer
+* Return: 0 Task done
+*/
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
