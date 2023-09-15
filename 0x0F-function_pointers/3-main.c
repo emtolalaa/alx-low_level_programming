169f46cba198bb4650c24690b2cf66d4b3678686
emtolalaa <emto.tochy@gmail.com>
2023-09-14 17:37:11 -0700

    Updated Task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char **argv)
 {
-	int n1, n2, result;
+	int num1, num2, result;
 	int (*res)(int, int);
 
 	if (argc != 4)
@@ -29,10 +29,10 @@ int main(int argc, char **argv)
 	if (res == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	result = res(n1, n2);
 	printf("%d\n", result);
 	return (0);
