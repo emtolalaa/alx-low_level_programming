b2e4bdcf41a5d4e94282befc8c8e94b6e5ea5b19
emtolalaa <emto.tochy@gmail.com>
2023-09-16 16:57:11 -0700

    Task update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,6 +5,7 @@
  * get_op_func - Selects the correct function to perform
  *               an operation rewuested by the user.
  * @s: an operator passed as an argument
+ *
  * Return: A pointer to the function which corresponds to the
  *         operator given as one of the parameters
  */
@@ -16,16 +17,15 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
+		{NULL, NULL},
 	};
+
 	int i;
 
 	i = 0;
-	while (i < 5)
-	{
-		if (*(ops[i].op) == *s && s[0] != '\0')
-			return (ops[i].f);
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (0);
+
+	return (ops[i].f);
 }
