77b00974647600595ec793925b660ba20873a7eb
emtolalaa <emto.tochy@gmail.com>
2023-09-16 17:14:19 -0700

    Task update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,31 +9,35 @@
  * @argv: an array of pointers to the arguments
  * Return: 0 For Task Done
  */
-int main(int argc, char **argv)
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*res)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	res = get_op_func(argv[2]);
-	if (res == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	result = res(num1, num2);
-	printf("%d\n", result);
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
